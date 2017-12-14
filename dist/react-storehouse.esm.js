db14b3ef40904b8fcdceee5128582956e30dfa1c
Michael Chan <mijoch@gmail.com>
2017-12-14 12:48:34 -0800

    convert to microbundle

--- dist/react-storehouse.esm.js
+++ dist/react-storehouse.esm.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
-class ReactStorehouse extends React.Component {
-  update(value) {
-    window.localStorage.setItem(this.props.item, JSON.stringify(value));
-    return this.forceUpdate();
-  }
-
-  render() {
-    if (this.props.render) {
-      return this.props.render(JSON.parse(window.localStorage.getItem(this.props.item)) || undefined, this.update.bind(this));
+var ReactStorehouse = (function (superclass) {
+    function ReactStorehouse () {
+        superclass.apply(this, arguments);
     }
 
-    return null;
-  }
-}
+    if ( superclass ) ReactStorehouse.__proto__ = superclass;
+    ReactStorehouse.prototype = Object.create( superclass && superclass.prototype );
+    ReactStorehouse.prototype.constructor = ReactStorehouse;
+
+    ReactStorehouse.prototype.update = function update (value) {
+        window.localStorage.setItem(this.props.item, JSON.stringify(value));
+        return this.forceUpdate();
+    };
+    ReactStorehouse.prototype.render = function render () {
+        if (this.props.render) {
+            return this.props.render(JSON.parse(window.localStorage.getItem(this.props.item)) || undefined, this.update.bind(this));
+        }
+        return null;
+    };
+
+    return ReactStorehouse;
+}(React.Component));
 
 export default ReactStorehouse;
+//# sourceMappingURL=react-storehouse.esm.js.map
--- dist/react-storehouse.umd.js
+++ dist/react-storehouse.umd.js
@@ -1,26 +1,2 @@
-(function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('react')) :
-	typeof define === 'function' && define.amd ? define(['react'], factory) :
-	(global.ReactStorehouse = factory(global.React));
-}(this, (function (React) { 'use strict';
-
-React = React && 'default' in React ? React['default'] : React;
-
-class ReactStorehouse extends React.Component {
-  update(value) {
-    window.localStorage.setItem(this.props.item, JSON.stringify(value));
-    return this.forceUpdate();
-  }
-
-  render() {
-    if (this.props.render) {
-      return this.props.render(JSON.parse(window.localStorage.getItem(this.props.item)) || undefined, this.update.bind(this));
-    }
-
-    return null;
-  }
-}
-
-return ReactStorehouse;
-
-})));
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?module.exports=e(require("react")):"function"==typeof define&&define.amd?define(["react"],e):t.react_storehouse=e(t.React)}(this,function(t){return function(t){function e(){t.apply(this,arguments)}return t&&(e.__proto__=t),e.prototype=Object.create(t&&t.prototype),e.prototype.constructor=e,e.prototype.update=function(t){return window.localStorage.setItem(this.props.item,JSON.stringify(t)),this.forceUpdate()},e.prototype.render=function(){return this.props.render?this.props.render(JSON.parse(window.localStorage.getItem(this.props.item))||void 0,this.update.bind(this)):null},e}((t=t&&t.hasOwnProperty("default")?t.default:t).Component)});
+//# sourceMappingURL=react-storehouse.umd.js.map
